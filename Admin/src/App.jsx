70ce4770a8f3c00ca4fcf3695b6553e3764e74d8
Kakul Agrawal <kakulagrawal32@gmail.com>
2026-07-15 17:00:13 +0530

    Add new asset images and implement Add, List, and Orders pages
    - Added new asset images: add_icon.png, logo.png, order_icon.png, parcel_icon.png, profile_image.png, and upload_area.png.
    - Created assets.js to manage asset imports and URLs.
    - Developed Add page with form for adding new products, including image upload functionality.
    - Implemented List and Orders pages with basic structure and styling.
    - Added CSS styles for Add, List, and Orders pages to enhance layout and appearance.

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import { Routes , Route } from 'react-router-dom'
+import List from './pages/List/List'
+import Add from './pages/Add/Add'
+import Orders from './pages/Orders/Orders'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
+
   return (
     <div>
-      
+      <Navbar/>
+      <ToastContainer/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
