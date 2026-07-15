97be6c4fca9d885dd72d91c236d4201ebd700d11
Kakul Agrawal <kakulagrawal32@gmail.com>
2026-07-15 11:53:01 +0530

    Add food management functionality with database connection and routes

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 //app config
@@ -9,6 +11,12 @@ const port=4000
 //middleware
 app.use(express.json())
 app.use(cors())
+
+//db connection
+connectDB();
+
+//api endpoint
+app.use("/api/food",foodRouter);
 
 app.get("/",(req,res)=>{
     res.send("API Working")
