851cc30f5ce49b77cb19b0a96b340ebc68e32e51
Kakul Agrawal <kakulagrawal32@gmail.com>
2026-07-16 00:15:08 +0530

    Implement user authentication with login and registration endpoints, add user model, and integrate dotenv for environment variables

---
+++ Server/models/userModel.js
@@ -0,0 +1,9 @@
+import mongoose, { mongo } from "mongoose";
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    cartData:{type:Object,default:{}}
+},{mininmize:false});
+const userModel=mongoose.models.user||mongoose.model("user",userSchema);
+export default userModel;
