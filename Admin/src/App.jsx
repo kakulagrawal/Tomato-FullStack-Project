d45d1d22280863aad7f71ed9121a0142529e06e4
Kakul Agrawal <kakulagrawal32@gmail.com>
2026-07-17 16:13:42 +0530

    Refactor App component to improve routing structure and add default redirect

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,31 +1,57 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Sidebar from './Components/Sidebar/Sidebar'
-import { Routes , Route } from 'react-router-dom'
-import List from './pages/List/List'
-import Add from './pages/Add/Add'
-import Orders from './pages/Orders/Orders'
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import List from "./pages/List/List";
+import Add from "./pages/Add/Add";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
 
-  const url="http://localhost:4000"
+    const url = "http://localhost:4000";
 
-  return (
-    <div>
-      <Navbar/>
-      <ToastContainer/>
-      <hr />
-      <div className="app-content">
-        <Sidebar/>
-        <Routes>
-          <Route path='/add' element={<Add url={url} />}/>
-          <Route path='/list' element={<List url={url} />}/>
-          <Route path='/orders' element={<Orders url={url} />}/>
-        </Routes>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div>
 
-export default App
+            <Navbar />
+
+            <ToastContainer />
+
+            <hr />
+
+            <div className="app-content">
+
+                <Sidebar />
+
+                <Routes>
+
+                    <Route
+                        path="/"
+                        element={<Navigate to="/add" replace />}
+                    />
+
+                    <Route
+                        path="/add"
+                        element={<Add url={url} />}
+                    />
+
+                    <Route
+                        path="/list"
+                        element={<List url={url} />}
+                    />
+
+                    <Route
+                        path="/orders"
+                        element={<Orders url={url} />}
+                    />
+
+                </Routes>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default App;
